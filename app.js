8fa9bd7a1d61c39ccd5ae2ae94b708636ed18b5c
Matthew Mogavero <matt.moga@gmail.com>
2018-01-30 20:45:38 -0500

    implemented the random name generation

--- app.js
+++ app.js
@@ -1,10 +1,32 @@
+//create your variables
 const button = document.querySelector('#generateBtn');
 const firstNameEl = document.querySelector('#firstName');
 const lastNameEl = document.querySelector('#lastName');
+const lastNames = [
+    'Gorgonzola',
+    'Mcgee',
+    'Pipsqueak',
+    'Wifey'
+];
+const firstNames = [
+    'Juicy',
+    'Snuggles',
+    'Gimpy',
+    'Smutty'
+];
 
+//add your eventListeners
 button.addEventListener('click', generateName);
 
+//functions
 function generateName(e) {
-    console.log(firstNameEl);
-    console.log(lastNameEl);
+    const rndLastName = randomChoice(lastNames);
+    const rndFirstName = randomChoice(firstNames);
+    firstNameEl.textContent = rndFirstName;
+    lastNameEl.textContent = rndLastName;
+}
+
+function randomChoice(array) {
+    const rndIndex = Math.floor(Math.random() * array.length);
+    return array[rndIndex];
 }
